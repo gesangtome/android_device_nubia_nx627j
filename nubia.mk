05e80581098cd9a38c2dca2d3b67769dd4225768
Jaekyun Seok <jaekyun@google.com>
2017-04-05 11:23:26 +0900

    sdm845-common: Enforce RRO on framework-res
    Overlays only for framework-res will be converted into RROs.
    
    Other overlays can't be converted due to some known issues on app RRO.
    
    Bug: 36231603
    Change-Id: Idca30fdbbcc990fd124de16a06a112a346612a61
    (cherry picked from commit db1b6058200fc5255f519cf6609fca7619d6743e)

--- nubia.mk
+++ nubia.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+PRODUCT_ENFORCE_RRO_TARGETS := framework-res
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
 	device/nubia/nx627j/overlay \

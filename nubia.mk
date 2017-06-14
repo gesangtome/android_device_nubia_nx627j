1f47f7a2e796213c013da8481211f87d970bef5e
Vinay Verma <vvinay@codeaurora.org>
2017-06-14 14:35:18 +0530

    sdm845-common: Add vndk sp libraries to system lib.
    Change-Id: Ia5b0087506571a929169736dfe108e7ff3f6d40f

--- nubia.mk
+++ nubia.mk
@@ -25,3 +25,23 @@ $(call inherit-product-if-exists, vendor/nubia/nx627j/nx627j-vendor.mk)
 # VNDK
 PRODUCT_PACKAGES += \
     vndk_package
+
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0.vndk-sp \
+    android.hardware.graphics.allocator@2.0.vndk-sp \
+    android.hardware.graphics.mapper@2.0.vndk-sp \
+    android.hardware.graphics.common@1.0.vndk-sp \
+    android.hidl.base@1.0.vndk-sp \
+    libhwbinder.vndk-sp \
+    libbase.vndk-sp \
+    libcutils.vndk-sp \
+    libhardware.vndk-sp \
+    libhidlbase.vndk-sp \
+    libhidltransport.vndk-sp \
+    libutils.vndk-sp \
+    libc++.vndk-sp \
+    libsync.vndk-sp \
+    libbacktrace.vndk-sp \
+    libunwind.vndk-sp \
+    liblzma.vndk-sp
+

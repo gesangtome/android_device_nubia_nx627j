8f897c8c771a7900bc2fe4141d35635bce245ff4
Demon Singur <demonsingur@gmail.com>
2018-06-23 05:22:18 +0000

    sdm845-common: Build separate system and vendor HIDL libs
    Change-Id: If8e2211e643bdb0ff67fb248e7fe33b78077a06e

--- nubia.mk
+++ nubia.mk
@@ -30,7 +30,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # LiveDisplay
 PRODUCT_PACKAGES += \

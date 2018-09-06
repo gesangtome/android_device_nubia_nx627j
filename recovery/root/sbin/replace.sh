2cde9488e732cf072b57d45a980d717a1c5948a3
notsyncing <song.fc@gmail.com>
2018-09-06 22:42:39 +0800

    Fix QSEECOM execution failure
    Dynamically replace QSEECOM's interpreter header
    Thanks to everyone in this issue: https://github.com/notsyncing/android_device_xiaomi_polaris/issues/13

---
+++ recovery/root/sbin/replace.sh
@@ -0,0 +1,24 @@
+#!/sbin/sh
+
+WORKSPACE=/sbin
+
+COPY=`which cp`
+REPLACE=`which sed`
+CHMOD=`which chmod`
+SETPROP=`which setprop`
+DELETE=`which rm`
+
+function replace()
+{
+    ${COPY} $1 ${1}_old
+    ${REPLACE} 's|/system/bin/linker64|///////sbin/linker64|' "${1}_old" > "$1"
+    ${DELETE} -rf ${1}_old
+    ${CHMOD} 755 $1
+}
+
+replace ${WORKSPACE}/qseecomd
+replace ${WORKSPACE}/android.hardware.gatekeeper@1.0-service-qti
+replace ${WORKSPACE}/android.hardware.keymaster@3.0-service-qti
+replace ${WORKSPACE}/android.hardware.keymaster@4.0-service-qti
+
+${SETPROP} crypto.ready 1

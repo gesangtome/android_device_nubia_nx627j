968156e3193294036fe1c2d2dd7b44f00dfbfe93
Jeff Tinker <jtinker@google.com>
2018-11-05 15:04:17 -0800

    sdm845-common: Enable clearkey drm plugin v1.2
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug:117570956
    bug:110838441
    bug:116252891
    Change-Id: Ie9373543ef416e718a70e75d95bf5b15a2852705

--- nubia.mk
+++ nubia.mk
@@ -78,7 +78,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 # Fstab
 PRODUCT_PACKAGES += \

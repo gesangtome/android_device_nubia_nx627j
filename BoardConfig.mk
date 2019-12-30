d573c3906dcc92b610c39173f2346bc25cf8b3e2
EvilGodEX <evilgodex@foxmail.com>
2019-12-30 12:05:49 +0800

    NX627J: Including more SELinux rules

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -222,6 +222,15 @@ VENDOR_SECURITY_PATCH := 2018-08-05
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/nubia/nx627j/sepolicy/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/nubia/nx627j/sepolicy/public
+
+BOARD_SEPOLICY_DIRS += \
+    device/nubia/nx627j/sepolicy/vendor
+
 # VNDK
 BOARD_VNDK_VERSION := current
 

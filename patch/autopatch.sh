a2ba13b576586881ba85904775edf02323580adf
gesangtome <gesangtome@foxmail.com>
2020-03-03 12:55:08 +0800

    NX627J: update autopatch script
     fix errors in execution
     more automated processes
    
    Signed-off-by: gesangtome <gesangtome@foxmail.com>

--- patch/autopatch.sh
+++ patch/autopatch.sh
@@ -1,17 +1,41 @@
-#!/bin/env bash
-# Created by 弱弱的胖橘猫丷
-# 2020.02.22 09:33:00
+#!/bin/env sh
+# Created by 弱弱的胖橘猫丷 <gesangtome@foxmail.com>
+# Last update: 2020.03.03 12:53
 
+audio=../../../../vendor/qcom/opensource/audio-hal/primary-hal/
+media=../../../../hardware/qcom/media/
 
 function applypatch() {
-    cp 0001-audio-Fix-build-problems.patch ../../../../vendor/qcom/opensource/audio-hal/primary-hal/
-    cd ../../../../vendor/qcom/opensource/audio-hal/primary-hal/
-    git am 0001-audio-Fix-build-problems.patch
-    cd -
-    cp 0001-media-fix-build-error.patch ../../../../hardware/qcom/media/
-    cd ../../../../hardware/qcom/media/
-    git am 0001-media-fix-build-error.patch
-    cd -
+
+    while [ $# != 0 ]; do
+          
+      [[ $1 =~ "audio-hal/primary-hal" ]] &&  patchfile=audio.patch
+      [[ $1 =~ "qcom/media" ]] &&  patchfile=media.patch
+
+      cp -a $patchfile $1
+      cd $1
+
+      ChangeID=`cat $patchfile | grep 'Change-Id:' | sed 's/Change-Id: //g'`
+      git log | grep $ChangeID >/dev/null 2>&1
+
+      case $? in
+        0)
+          printf "The patch has been applied to the workspace, skiping ...\n"
+          rm -rf $patchfile
+          cd - >/dev/null 2>&1
+          ;;
+        *)
+          printf "The patch was not applied to the workspace, applying ...\n"
+          git am $patchfile 2> /dev/null
+          rm -rf $patchfile
+          cd - >/dev/null 2>&1
+          ;;
+      esac
+          shift;
+    done
 }
 
-  applypatch
+  applypatch $audio $media
+
+  unset audio
+  unset media

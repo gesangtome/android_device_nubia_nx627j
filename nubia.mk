6a2eefa8a62ab4a366b3b9a02fdc6805d252248b
gesangtome <gesangtome@foxmail.com>
2020-03-23 23:16:44 +0800

    NX627J: Custom property inheritance
    Signed-off-by: gesangtome <gesangtome@foxmail.com>

--- nubia.mk
+++ nubia.mk
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product-if-exists, vendor/nubia/nx627j/nx627j-vendor.mk)
+
 

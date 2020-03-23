52c5353440416f15de306a2848233a299de77227
gesangtome <gesangtome@foxmail.com>
2020-03-24 03:16:39 +0800

    NX627J: update patch
    Signed-off-by: gesangtome <gesangtome@foxmail.com>
    Change-Id: I20ac69499b9fd68788ccacc31a597e8ece4eeaab

--- patch/README.md
+++ patch/README.md
@@ -1,7 +1,7 @@
 # This folder contains patches that can help resolve compilation errors
 
 
-0001-media-fix-build-error.patch --> hardware/qcom-caf/sm8150/audio/
+0001-audio-Fix-build-problems.patch --> vendor/qcom/sm8150/opensource/audio-hal/primary-hal
 
 
-0001-media-fix-build-error.patch --> hardware/qcom-caf/sm8150/media/
+0001-media-fix-build-error.patch --> hardware/qcom/media/
--- patch/autopatch.sh
+++ patch/autopatch.sh
@@ -1,15 +1,15 @@
 #!/bin/env bash
 # Created by 弱弱的胖橘猫丷
-# 2020.01.15 08:48:00
+# 2020.02.22 09:33:00
 
 
 function applypatch() {
-    cp 0001-audio-Fix-compilation-error.patch ../../../../hardware/qcom-caf/sm8150/audio
-    cd ../../../../hardware/qcom-caf/sm8150/audio
-    git am 0001-audio-Fix-compilation-error.patch
+    cp 0001-audio-Fix-build-problems.patch ../../../../hardware/qcom-caf/sm8150/audio/
+    cd ../../../../hardware/qcom-caf/sm8150/audio/
+    git am 0001-audio-Fix-build-problems.patch
     cd -
-    cp 0001-media-fix-build-error.patch ../../../../hardware/qcom-caf/sm8150/media
-    cd ../../../../hardware/qcom-caf/sm8150/media
+    cp 0001-media-fix-build-error.patch ../../../../hardware/qcom-caf/sm8150/media/
+    cd ../../../../hardware/qcom-caf/sm8150/media/
     git am 0001-media-fix-build-error.patch
     cd -
 }

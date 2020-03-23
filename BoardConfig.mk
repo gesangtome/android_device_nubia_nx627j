8775707a08659f382c594ce509300e39e3c85e22
gesangtome <gesangtome@foxmail.com>
2020-03-23 23:12:47 +0800

    NX627J: Define cpu variants
    Signed-off-by: gesangtome <gesangtome@foxmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,5 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a75
+
+TARGET_USES_64_BIT_BINDER := true
+
 # inherit from the proprietary version
 -include vendor/nubia/nx627j/BoardConfigVendor.mk

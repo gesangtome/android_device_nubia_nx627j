c67616bb9723e00a815384828852fe58373a5944
gesangtome <gesangtome@foxmail.com>
2020-03-23 23:17:24 +0800

    NX627J: Add lunch options

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+#
+# Copyright (c) 2019, 弱弱的胖橘猫丷
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+for manifests in eng user userdebug; do
+  add_lunch_combo lineage_nx627j-$manifests
+done

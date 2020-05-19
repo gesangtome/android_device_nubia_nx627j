d5a2815f02d8909e897bf33f229f9f77ba1b74d1
gesangtome <gesangtome@foxmail.com>
2020-05-19 18:40:27 +0800

    Add dts patch | update applypatch.sh
    Signed-off-by: gesangtome <gesangtome@foxmail.com>
    Change-Id: I5dc26576d5ce5212c2a64e38789152a06d1dff68

--- patch/autopatch.sh
+++ patch/autopatch.sh
@@ -1,40 +1,62 @@
 #!/bin/env sh
 # Created by 弱弱的胖橘猫丷 <gesangtome@foxmail.com>
-# Last update: 2020.03.03 12:53
+# Last update: 2020.05.19 18:14
 
-audio=../../../../hardware/qcom-caf/sm8150/audio/
-media=../../../../hardware/qcom-caf/sm8150/media/
+audio_path=../../../../hardware/qcom-caf/sm8150/audio/
+media_path=../../../../hardware/qcom-caf/sm8150/media/
 
-function applypatch() {
-    while [ $# != 0 ]; do
-          
-      [[ $1 =~ "audio" ]] &&  patchfile=audio.patch
-      [[ $1 =~ "media" ]] &&  patchfile=media.patch
-
-      cp -a $patchfile $1
-      cd $1
-
-      ChangeID=`cat $patchfile | grep 'Change-Id:' | sed 's/Change-Id: //g'`
-      git log | grep $ChangeID >/dev/null 2>&1
-
-      case $? in
-        0)
-          printf "The patch has been applied to the workspace, skiping ...\n"
-          rm -rf $patchfile
-          cd - >/dev/null 2>&1
-          ;;
-        *)
-          printf "The patch was not applied to the workspace, applying ...\n"
-          git am $patchfile 2> /dev/null
-          rm -rf $patchfile
-          cd - >/dev/null 2>&1
-          ;;
-      esac
-          shift;
-    done
+function copy_patch_files()
+{
+	for files_name in *.patch;
+	do
+		[[ $files_name =~ "dts_eagle" ]] && cp -a $files_name $audio_path/
+		[[ $files_name =~ "audio" ]] && cp -a $files_name $audio_path/
+		[[ $files_name =~ "media" ]] && cp -a $files_name $media_path/
+	done
 }
 
-  applypatch $audio $media
+function apply_patch_files() {
+	while [ $# != 0 ];
+	do
+		local local_path=$1
 
-  unset audio
-  unset media
+		cd $local_path
+		for files in *.patch;
+		do
+			local change=`cat $files | grep 'Change-Id:'| sed 's/^Change-Id: //g'`
+			git log | grep $change >/dev/null 2>&1
+
+			case $? in
+				0)
+					echo "This patch has been applied: $files"
+					rm -rf $files
+					;;
+				*)
+					git apply --check $files >/dev/null 2>&1
+					case $? in
+						0)
+							echo "Applying the patch: $files"
+							git am $files >/dev/null 2>&1 && rm -rf $files
+							;;
+						*)
+							echo "Can not apply the patch: $files"
+							rm -rf $files
+							;;
+					esac
+					;;
+			esac
+		done
+		cd - >/dev/null 2>&1
+		shift
+	done
+}
+
+# copy patch files
+copy_patch_files
+
+# apply patch files
+apply_patch_files $audio_path $media_path
+
+# unset 
+unset audio_path
+unset media_path
